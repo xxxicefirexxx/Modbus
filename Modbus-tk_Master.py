53ce166ebe413f28f5d45959b004f51fd8b686d4
xxxicefirexxx <dia.icefire@gmail.com>
2017-02-24 00:14:22 +0800

    Add files via upload

---
+++ Modbus-tk_Master.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+# -*- coding: utf_8 -*-
+"""
+ Modbus TestKit: Implementation of Modbus protocol in python
+
+ (C)2009 - Luc Jean - luc.jean@gmail.com
+ (C)2009 - Apidev - http://www.apidev.fr
+
+ This is distributed under GNU LGPL license, see license.txt
+"""
+
+from __future__ import print_function
+
+import modbus_tk
+import modbus_tk.defines as cst
+from modbus_tk import modbus_tcp, hooks
+
+
+def main():
+    """main"""
+    logger = modbus_tk.utils.create_logger("console")
+
+    def on_after_recv(data):
+        master, bytes_data = data
+        print(bytes_data)
+
+    hooks.install_hook('modbus.Master.after_recv', on_after_recv)
+
+    try:
+
+        def on_before_connect(args):
+            master = args[0]
+            print("on_before_connect", master._host, master._port)
+
+        hooks.install_hook("modbus_tcp.TcpMaster.before_connect", on_before_connect)
+
+        def on_after_recv(args):
+            response = args[1]
+            print("on_after_recv", len(response), "bytes received")
+
+        hooks.install_hook("modbus_tcp.TcpMaster.after_recv", on_after_recv)
+
+        # Connect to the slave
+        master = modbus_tcp.TcpMaster(host='192.168.1.100') #Set Slave IP Address
+        master.set_timeout(5.0)
+        logger.info("connected")
+        
+        logger.info(master.execute(1, cst.READ_HOLDING_REGISTERS, 0, 5)) # Set Start Address & Block Size
+
+        # send some queries
+        # logger.info(master.execute(1, cst.READ_COILS, 0, 10))
+        # logger.info(master.execute(1, cst.READ_DISCRETE_INPUTS, 0, 8))
+        # logger.info(master.execute(1, cst.READ_INPUT_REGISTERS, 100, 3))
+        # logger.info(master.execute(1, cst.READ_HOLDING_REGISTERS, 100, 12))
+        # logger.info(master.execute(1, cst.WRITE_SINGLE_COIL, 7, output_value=1))
+        # logger.info(master.execute(1, cst.WRITE_SINGLE_REGISTER, 100, output_value=54))
+        # logger.info(master.execute(1, cst.WRITE_MULTIPLE_COILS, 0, output_value=[1, 1, 0, 1, 1, 0, 1, 1]))
+        # logger.info(master.execute(1, cst.WRITE_MULTIPLE_REGISTERS, 100, output_value=xrange(12)))
+
+    except modbus_tk.modbus.ModbusError as exc:
+        logger.error("%s- Code=%d", exc, exc.get_exception_code())
+
+if __name__ == "__main__":
+    main()
